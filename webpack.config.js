55059568e88358930b71feb665805d1d5f7f801a
yacyam <119266083+yacyam@users.noreply.github.com>
2023-01-03 19:05:56 -0500

    created live server template

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devServer: {
+    static: {
+        directory: path.resolve(__dirname, 'dist')
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: 'Simple Restaurant',
+        filename: 'index.html',
+        template: 'src/template.html'
+    })
+  ]
 };
